5e690957448510a3878d4ef47bf9c3a25a485ef9
Asya Yeromina <asyayeromina@gmail.com>
2020-09-14 09:13:18 +0300

    Fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -26,11 +26,20 @@ const styles = () => {
     .pipe(csso())
     .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("./build/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+// HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(gulp.dest("./build"))
+  }
+
+  exports.html = html;
 
 
 // Imagemin
@@ -41,7 +50,8 @@ const images = () => {
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.mozjpeg({progressive: true}),
       imagemin.svgo()
-    ]));
+    ]))
+    .pipe(gulp.dest("build/img"));
 }
 
 exports.images = images;
@@ -67,26 +77,19 @@ const sprite = () => {
 
   exports.sprite = sprite;
 
-// HTML
-
-const html = () => {
-  return gulp.src("source/*.html")
-    .pipe(gulp.dest("build/"))
-  }
-
-  exports.html = html;
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'build'
+      baseDir: "./build"
     },
     cors: true,
     notify: false,
     ui: false,
   });
+
   done();
 }
 
@@ -95,24 +98,25 @@ exports.server = server;
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html", gulp.series("html"));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+  gulp.watch("source/*.html", gulp.series(html));
 }
 
 exports.default = gulp.series(
   styles, server, watcher
 );
 
-// Build
+// Copy to build
 const copy = () => {
   return gulp.src([
-  "source/fonts/**/*.{woff,woff2}",
-  "source/img/**",
-  "source/js/**",
-  "source/*.ico"
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.ico"
   ], {
-  base: "source"
-  }) .pipe(gulp.dest("build"));
+    base: "source"
+  })
+    .pipe(gulp.dest("build"));
 };
 
   exports.copy = copy;
@@ -127,6 +131,11 @@ exports.clean = clean;
 
 
 // Build new
-const build = gulp.series(clean, copy, styles, sprite, html, server);
+const build = gulp.series(clean, copy, styles, images, sprite, html);
 
 exports.build = build;
+
+
+// Start
+
+exports.start = gulp.series(build, server, watcher);
