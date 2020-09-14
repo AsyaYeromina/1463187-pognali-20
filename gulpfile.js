0614fe1fd9473373e9566e3917f1ba9cc522925a
Asya Yeromina <asyayeromina@gmail.com>
2020-09-14 09:25:46 +0300

    Fixed gulpfile (added css file to build/)

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ const styles = () => {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("./build/css"))
     .pipe(csso())
     .pipe(rename("styles.min.css"))
     .pipe(sourcemap.write("."))
